3d261119f73e399d6f8c2cdb48c8be2f2237d659
luk3skyw4lker <48929501+luk3skyw4lker@users.noreply.github.com>
2019-10-29 14:00:57 -0300

    Adding create/auth user and testing

---
+++ backend/test/functional/session.spec.js
@@ -0,0 +1,23 @@
+const { test, trait } = use('Test/Suite')('Session');
+
+/** @type {import('@adonisjs/lucid/src/Factory')} **/
+const Factory = use('Factory')
+
+trait('Test/ApiClient');
+
+test('it should return JWT token when session created', async ({ assert, client }) => {
+    const sessionPayload = {
+        email: 'lucashenriqueblemos@gmail.com',
+        password: '123456'
+    }
+
+    const user = await Factory.model('App/Models/User').create(sessionPayload);
+
+    const response = await client
+        .post('/sessions')
+        .send(sessionPayload)
+        .end();
+    
+    response.assertStatus(200);
+    assert.exists(response.body.token);
+});
